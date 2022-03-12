1ab4a7aaf2b3b5ae271808fabecae660dd265b08
Yuliia Dudii <partner.Yuliia.Dudii@philips.com>
2022-03-12 18:34:02 +0200

    #2 - Init web repo.

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from "@angular/router";
+import { TransfersComponent } from "./transfer-builder/transfers/transfers.component";
+import { GroupsComponent } from "./group-builder/groups/groups.component";
+import { StatisticsComponent } from "./statistic-builder/statistics/statistics.component";
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/my-wallet',
+    pathMatch: 'full'
+  },
+  {
+    path: 'my-wallet',
+    component: TransfersComponent,
+  },
+  {
+    path: 'my-groups',
+    component: GroupsComponent,
+  },
+  {
+    path: 'statistics',
+    component: StatisticsComponent,
+  },
+]
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { PortalComponent } from './portal-layout/portal/portal.component';
+import { FooterComponent } from './portal-layout/footer/footer.component';
+import { NavbarComponent } from './portal-layout/navbar/navbar.component';
+import { AppRoutingModule } from './app-routing.module';
+import { TransfersComponent } from './transfer-builder/transfers/transfers.component';
+import { GroupsComponent } from './group-builder/groups/groups.component';
+import { StatisticsComponent } from './statistic-builder/statistics/statistics.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PortalComponent,
+    FooterComponent,
+    NavbarComponent,
+    TransfersComponent,
+    GroupsComponent,
+    StatisticsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
